e0a63575cfd620be9e56cbd5c81fa65b83507b35
Micahel Turner <aztex87068@juno.com>
2015-10-16 12:00:55 -0600

    first commit for index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>mr-porter product page</title>
+	</head>
+	<body>
+		<h1>persona</h1>
+		<p>
+			Dylan is an affluent male, 27-56 years old, urban dweller, status & quality driven, he know his sizing, knows brands & wants products shipped  to his home or friends.a business owner/executive,very tech savy likes a faster -sleeker form like Apple, uses an mobile device or tablet( IPhone 6s plus & IPad pro ).</p>
+		<h1>use case 1</h1>
+		<ul>
+			<li>goal #1 looking for a blazer.</li>
+			<li>step 1 goes to mr-porter.com</li>
+			<li>step 2 looks at clothing from top nav bar</li>
+			<li>step 3selects blazers from the drop down menu</li>
+			<li>the displays a list of blazers</li>
+			<li>step 4 selects a specific blazer item</li>
+			<li>step 5 this step allows for more detailed info</li>
+			<li> allows me to buy from given info.</li>
+		</ul>
+		<h1>test</h1>
+		<h1>Conceptual Model</h1>
+		<ul>
+
+
+
+		</ul>
+		<strong>Entity: Brand</strong>
+		<ul>
+			<li>Brand ID</li>
+			<li>Brand Name</li>
+			<li>Description</li>
+			<li>Products</li>
+		</ul>
+		<p><strong>Relationship:</strong> Each Brand can have many Products. (One to many relationship.)</p>
+		<strong>Entity:Product</strong>
+		<ul>
+			<li>Brand</li>
+			<li>Product  ID</li>
+			<li>Description</li>
+			<li>Price</li>
+			<li>catagorie</li>
+			<name></name>
+		</ul>
+		<p>Each product can have only one brand. (one to one relationship.)</p>
+
+
+	</body>
+</html>
